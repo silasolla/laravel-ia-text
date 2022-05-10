8fc26f0f5fcf51fd681e04399341d5d865bb389f
Masaki Haga <m.haga@sonicmoov.com>
2022-05-10 18:30:54 +0900

    edit controller

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Text;
 
 class TextController extends Controller
 {
-    //
+	public function index(){
+		$texts = Text::all();
+		// dd($texts); // dump + die
+		return view('texts.index', compact('texts'));
+	}
+	public function create(){
+		return view('texts.create');
+	}
 }

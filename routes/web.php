335cf6d824a1a3770bd7e8c16a27a14eb53e22c4
Masaki Haga <m.haga@sonicmoov.com>
2022-05-11 17:33:13 +0900

    add routes

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ Route::get('/', function () {
 Route::get('/texts/index', [TextController::class, 'index'])->name('texts.index');
 Route::get('/texts/create', [TextController::class, 'create'])->name('texts.create');
 Route::post('/texts/store', [TextController::class, 'store'])->name('texts.store');
+
+Route::get('/texts/{id}', [TextController::class, 'show'])->name('texts.show');
+Route::get('/texts/{id}/edit', [TextController::class, 'edit'])->name('texts.edit');
+Route::post('/texts/{id}', [TextController::class, 'update'])->name('texts.update');
+Route::post('/texts/{id}/delete', [TextController::class, 'delete'])->name('texts.delete');

ba96c064bb82a37caf96b46106173c1440e1b065
Masaki Haga <m.haga@sonicmoov.com>
2022-05-13 18:43:50 +0900

    create lecture view

--- app/Http/Controllers/LectureController.php
+++ app/Http/Controllers/LectureController.php
@@ -5,13 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\Lecture;
+use App\Models\User;
+
 class LectureController extends Controller
 {
     public function index()
 	{
-        $user = Auth::user();
-        $userId = Auth::id();
-
+        $authUser   = Auth::user();
+        $authUserId = Auth::id();
         // dd($user, $userId);
+		
+		$user = User::find($authUserId);
+		// dd($user);
+		foreach ($user->lectures as $lecture){
+			dd($lecture->pivot);
+		}
+		$lectureId = Lecture::all();
+		
+		return view('lectures.index', compact('authUser'));
     }
 }

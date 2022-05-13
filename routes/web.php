95bfa628654a826d24ddbcebfa2f5eb07168e1ed
Masaki Haga <m.haga@sonicmoov.com>
2022-05-13 17:20:22 +0900

    create lecture controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TextController;
+use App\Http\Controllers\LectureController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,4 +24,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::middleware(['auth'])->group(function(){
+    Route::get('/lectures/', [LectureController::class, 'index'])->name('lectures.index');    
+});
+
 require __DIR__.'/auth.php';
+
+Route::get('/texts/', [TextController::class, 'index'])->name('texts.index');
+Route::get('/texts/create', [TextController::class, 'create'])->name('texts.create');
+Route::post('/texts/store', [TextController::class, 'store'])->name('texts.store');
+Route::get('/texts/{id}', [TextController::class, 'show'])->name('texts.show');
+Route::get('/texts/{id}/edit', [TextController::class, 'edit'])->name('texts.edit');
+Route::post('/texts/{id}', [TextController::class, 'update'])->name('texts.update');
+Route::post('/texts/{id}/delete', [TextController::class, 'delete'])->name('texts.delete');
